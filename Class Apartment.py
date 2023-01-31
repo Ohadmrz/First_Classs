8984245a67c3ada0a1761566aa299930ce385782
Ohadmrz <117463898+Ohadmrz@users.noreply.github.com>
2023-01-31 15:51:09 +0200

    Add files via upload

---
+++ Class Apartment.py
@@ -0,0 +1,13 @@
+class Apartment:
+
+    def __init___(self, address: dict, gira: dict, rooms: dict,
+                  bathroom: dict, kitchen_size: int, balconies: dict = 0 ):
+        self._addred: dict[str, str] = {}
+        self._gira: dict[str, int] = {}
+        self._rooms: dict[int,int] = {}
+        self._bathroom: dict[int,int] = {}
+        self._kitchen_size: int = kitchen_size
+        self._balconies: dict[int,int] =  balconies
+        self._total_size: int = roomms + bathroom + kitchen_size + balconies
+
+
